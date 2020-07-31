22c1297790d6fd0629ea95c77dca348f4a42bd6a
manish-1305 <62827309+manish-1305@users.noreply.github.com>
2020-07-31 13:15:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def predict():
         else:
             Transmission_Mannual=0
         prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
-        output=round(prediction[0],2)
+        output=round(prediction[0]//10,2)
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
         else:
