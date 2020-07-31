d531db62441b8ceb3d332300da929cfcfa12aa3b
manish-1305 <62827309+manish-1305@users.noreply.github.com>
2020-07-31 14:45:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def predict():
     Fuel_Type_Diesel=0
     if request.method == 'POST':
         Year = int(request.form['Year'])
-        Present_Price=float(request.form['Present_Price'])
+        Present_Price=float((request.form['Present_Price'])/100000)
         Kms_Driven=int(request.form['Kms_Driven'])
         Kms_Driven2=np.log(Kms_Driven)
         Owner=int(request.form['Owner'])
@@ -44,8 +44,8 @@ def predict():
         else:
             Transmission_Mannual=0
         prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
-        output=round(prediction[0]//10,2)
-        if output<0.5:
+        output=round(prediction[0],2)
+        if output<0:
             return render_template('index.html',prediction_text="Sorry you cannot sell this car")
         else:
             return render_template('index.html',prediction_text="You Can Sell The Car at {}(in lakhs)".format(output))
